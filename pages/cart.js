65e64d3d97c79f5fa0d961ee3e8b74a7916cbaa9
shayanali4 <shayana803@gmail.com>
2022-06-03 00:41:12 +0500

    cart issue fixed

--- pages/cart.js
+++ pages/cart.js
@@ -1,29 +1,36 @@
 import { useState, useContext, useEffect } from "react";
-import Image from 'next/image'
-import { getCountContext, setCountContext } from "../pages/_app.js"
-import Dropdown from '../components/Dropdown'
+import Image from "next/image";
+import { getCountContext, setCountContext } from "../pages/_app.js";
+import Dropdown from "../components/Dropdown";
 export default function cart() {
-  let getCount = useContext(getCountContext)
-  let setCount = useContext(setCountContext)
+  let getCount = useContext(getCountContext);
+  let setCount = useContext(setCountContext);
   const [products, setProducts] = useState([]);
-  const [qty, setQty] = useState(0);
 
-  useEffect(() => { // get data 
-    setProducts(JSON.parse(localStorage.getItem("products")))
-  }, [])
-
-  useEffect(() => {
-    if (localStorage.getItem('products') !== null) {
-    setCount(JSON.parse(localStorage.getItem('products')).length)
-    }
-  })
+  const changeQty = (item, qty) => {
+    setProducts(
+      products.map((x) =>
+        x.id === item.id ? { ...item, qty: parseInt(qty) } : x
+      )
+    );
+  };
 
   useEffect(() => {
-    localStorage.setItem("products",JSON.stringify(products))
-  },[products])
+    // get data
+    setProducts(JSON.parse(localStorage.getItem("products")));
+  }, []);
 
+  // useEffect(() => {
+  //   if (localStorage.getItem('products') !== null) {
+  //   setCount(JSON.parse(localStorage.getItem('products')).length)
+  //   }
+  // })
 
-  
+  useEffect(() => {
+    localStorage.setItem("products", JSON.stringify(products));
+    setCount(products.length)
+  }, [products]);
+
   return (
     <div className="Product-Container">
       {products.map((product, i) => {
@@ -34,21 +41,46 @@ export default function cart() {
               alt="Image Unavailable"
               src={product.image}
               width={300}
-              height={300} />
+              height={300}
+            />
             <h4 className="text-sm text-gray-700">{product.title}</h4>
             <h5 className="text-lg font-medium ">${product.price}</h5>
-            <h6 className="no-underline hover:no-underline">{product.rate}/5 of {product.count} Reviews</h6> {/*Add stars to */}
-            <button className="bg-black-500 hover:bg-gray-400 text-black font-bold py-2 px-4 rounded-full"
+            <h6 className="no-underline hover:no-underline">
+              {product.rate}/5 of {product.count} Reviews
+            </h6>{" "}
+            {/*Add stars to */}
+            <button
+              className="bg-black-500 hover:bg-gray-400 text-black font-bold py-2 px-4 rounded-full"
               onClick={() => {
-                setProducts(products.filter((x) => x.id !== product.id))//filters out by product id clicked
-              }}>x</button>
-             <Dropdown/>  
-  
+                setProducts(products.filter((x) => x.id !== product.id)); //filters out by product id clicked
+              }}
+            >
+              x
+            </button>
+            {/* <Dropdown/>   */}
+            <div>
+              <span>Qty:</span>
+              <select
+                name="quantity"
+                id="qty"
+                onChange={(e) => changeQty(product, e.target.value)}
+                value={product.qty}
+              >
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+                <option value={4}>4</option>
+                <option value={5}>5</option>
+                <option value={6}>6</option>
+                <option value={7}>7</option>
+                <option value={8}>8</option>
+                <option value={9}>9</option>
+                <option value={10}>10</option>
+              </select>
+            </div>
           </div>
-        )
+        );
       })}
-
     </div>
-  )
-
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,78 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Image from 'next/image'
-import Link from 'next/link'
-import { useContext, useEffect, useState } from 'react';
-import { getCountContext, setCountContext } from "../pages/_app.js"
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import { useContext, useEffect, useState } from "react";
+import { getCountContext, setCountContext } from "../pages/_app.js";
 
-
-
-
-export async function getStaticProps() { // called once at page reload to fetch store data
-  const res = await fetch('https://fakestoreapi.com/products')
-  const data = await res.json()
+export async function getStaticProps() {
+  // called once at page reload to fetch store data
+  const res = await fetch("https://fakestoreapi.com/products");
+  const data = await res.json();
   return {
-    props: { // will be passed to page component as props
+    props: {
+      // will be passed to page component as props
       data, // data is  a local variable in getStaticProps from the fetch
     },
-  }
+  };
 }
 
 export default function Home({ data }) {
-  const [carter, setCarter] = useState([]) // add to cart list saved here
+  const [carter, setCarter] = useState([]); // add to cart list saved here
 
+  let setCount = useContext(setCountContext);
+  let getCount = useContext(getCountContext);
 
-  let setCount = useContext(setCountContext)
-  let getCount = useContext(getCountContext)
-
-  useEffect(() => { // at initial launch get products from local storage and store in carter
-    localStorage.clear();
-    if (localStorage.getItem('products') !== null) {
-      setCarter(JSON.parse(localStorage.getItem('products')))
-      setCount(JSON.parse(localStorage.getItem('products')).length);
+  const addToCart = (product) => {
+    let productDetails = {
+      image: product.image,
+      id: product.id,
+      title: product.title,
+      rate: product.rating.rate,
+      count: product.rating.count,
+      price: product.price,
+      qty: 1,
+    };
+    const productExist = carter.find((x) => x.id === productDetails.id);
+    if (productExist) {
+      setCarter(
+        carter.map((x) =>
+          x.id === productExist.id
+            ? { ...productExist, qty: productExist.qty + 1 }
+            : x
+        )
+      );
+    } else {
+      setCarter([...carter, { ...productDetails, qty: 1 }]);
     }
-  }, [])
 
-  useEffect(() => { // anytime there is a change to carter i want it to update the localstorage with carter
-    localStorage.setItem('products', JSON.stringify(carter))
-    setCount(JSON.parse(localStorage.getItem('products')).length); //update cart count in navbar
-  }, [carter])
+    // //let clickCount = 1
+    // let productIsInCart = false
+    // carter.forEach(x => {    //if the product is already in your cart then add to qty dropdown in cart
+    //   if (x.id === product.id) {
+    //     // localStorage.setItem('count', clickCount++)
+    //     productIsInCart = true
+    //   }
+    // })
+    // if (!productIsInCart) {
+    //   setCarter([...carter, productDetails])
+    // }
+  };
+  console.log("cart==>", carter);
+
+  useEffect(() => {
+    // at initial launch get products from local storage and store in carter
+    // localStorage.clear();
+    if (localStorage.getItem("products") !== null) {
+      setCarter(JSON.parse(localStorage.getItem("products")));
+    }
+  }, []);
+
+  useEffect(() => {
+    // anytime there is a change to carter i want it to update the localstorage with carter
+    localStorage.setItem("products", JSON.stringify(carter));
+    setCount(carter.length)
+  }, [carter]);
 
   return (
     <div className={styles.container}>
@@ -50,44 +86,29 @@ export default function Home({ data }) {
           <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
             {data.map((product, i) => (
               <div key={i} className="productContainer">
-
                 <Link href={`/product/${product.id}`}>
-                  <a className="hover:opacity-80 hover:underline"> {/*Create dynamic links based on wahts clicked */}
+                  <a className="hover:opacity-80 hover:underline">
+                    {" "}
+                    {/*Create dynamic links based on wahts clicked */}
                     <Image
                       className=""
                       alt="Image Unavailable"
                       src={product.image}
                       width={300}
-                      height={300} />
+                      height={300}
+                    />
                     <h4 className="text-sm text-gray-700">{product.title}</h4>
                     <h5 className="text-lg font-medium ">${product.price}</h5>
-                    <h6 className="no-underline hover:no-underline">{product.rating.rate}/5 of {product.rating.count} Reviews</h6> {/*Add stars to */}
+                    <h6 className="no-underline hover:no-underline">
+                      {product.rating.rate}/5 of {product.rating.count} Reviews
+                    </h6>{" "}
+                    {/*Add stars to */}
                   </a>
                 </Link>
-                <button onClick={() => {
-
-                  let productDetails = {
-                    image: product.image,
-                    id: product.id,
-                    title: product.title,
-                    rate: product.rating.rate,
-                    count: product.rating.count,
-                    price: product.price
-                  }
-
-                 //let clickCount = 1
-                  let productIsInCart = false
-                  carter.forEach(x => {    //if the product is already in your cart then add to qty dropdown in cart 
-                    if (x.id === product.id) {
-                     // localStorage.setItem('count', clickCount++)
-                      productIsInCart = true
-                    }
-                  })
-                  if (!productIsInCart) {
-                    setCarter([...carter, productDetails])
-                  }
-
-                }} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+                <button
+                  onClick={() => addToCart(product)}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+                >
                   Add To Cart
                 </button>
               </div>
@@ -96,5 +117,5 @@ export default function Home({ data }) {
         </div>
       </div>
     </div>
-  )
+  );
 }
