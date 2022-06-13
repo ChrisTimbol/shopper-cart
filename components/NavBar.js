2edc6d3d8c61676f6ee8245e6ec00e3f4af864f9
Chris Timbol <christimbol707@gmail.com>
2022-06-13 15:19:28 -0700

    vercel changes

--- components/NavBar.js
+++ components/NavBar.js
@@ -11,7 +11,7 @@ export default function NavBar() {
             <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
                 <div className="container flex flex-wrap justify-between items-center mx-auto">
                     <Link href="/">
-                        <a href="/" className="flex items-center">
+                        <a  className="flex items-center">
                             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">ShopperCart</span>
                         </a>
                     </Link>
