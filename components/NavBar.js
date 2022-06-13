2c5b39895319931e259b39ea381ce9d63bae4ebd
Chris Timbol <christimbol707@gmail.com>
2022-06-13 15:23:52 -0700

    adjust navbar Link hrefs

--- components/NavBar.js
+++ components/NavBar.js
@@ -11,8 +11,8 @@ export default function NavBar() {
             <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
                 <div className="container flex flex-wrap justify-between items-center mx-auto">
                     <Link href="/">
-                        <a  className="flex items-center">
-                            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">ShopperCart</span>
+                        <a className="flex items-center self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+                            ShopperCart
                         </a>
                     </Link>
                     <button data-collapse-toggle="mobile-menu" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu" aria-expanded="false">
@@ -23,14 +23,14 @@ export default function NavBar() {
                     <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
                         <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
                             <li>
-                                <Link href="/">
-                                    <a className="block py-2 text-lg pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Shop</a>
+                                <Link href="/" className="block py-2 text-lg pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                    <a>Shop</a>
                                 </Link>
                             </li>
                             <li>
                                 <Link href="/cart">
                                     <a className="block   text-lg hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">&#128722;
-                                        <span className="text-base"> {getTotal}</span></a>
+                                        {getTotal}</a>
                                 </Link>
                             </li>
                         </ul>
