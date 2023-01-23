9edeeff343ed00d8d4e306728de7f75009d7a81c
Chris Timbol <christimbol707@gmail.com>
2023-01-23 14:10:53 -0800

    fix footer problem colliding with items

--- components/Footer.js
+++ components/Footer.js
@@ -3,11 +3,11 @@
 export default function Footer() {
     return (
 
-        <footer className="flex w-screen   p-4 bg-white rounded-lg shadow md:px-6 md:py-8 bg-purple-800">
+        <footer className="w-screen  mt-auto flex-end p-4 bg-white rounded-lg bg-purple-800">
             <div className="sm:flex sm:items-center sm:justify-between">
                 <span className="self-center text-xl font-semibold whitespace-nowrap text-white">ShopperCart &#128722;</span>
             </div>
-                <div className="block text-sm sm:text-center text-white">© 2022 ShopperCart. All Rights Reserved.
+                <div className="text-sm sm:text-center text-white">© 2022 ShopperCart. All Rights Reserved.
                 </div>
         </footer>
 
